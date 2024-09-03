f51fce6842a2e8e4f52a838800839f665d42e4b2
Ilyes Aljane <aljaneelyes07@gmail.com>
2024-09-03 16:50:59 +0100

    code source

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+import catgif from "C:\Users\USER\react-deploy\src\cat-gif-loop-maru_grande.webp"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={catgif} alt="" />
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit totam vel tempore? Ab, rem veritatis quibusdam consequuntur maxime voluptatibus in iure! Laudantium, facere suscipit? Voluptas ea dignissimos omnis ducimus porro neque nesciunt adipisci enim dolores ut, ab a veritatis quod aspernatur eius. Reiciendis magni excepturi ex, ipsa cumque, amet ullam deleniti animi nesciunt hic earum quis vel officiis ipsum at debitis sapiente neque corporis illo harum provident odio quo praesentium? Modi, obcaecati quisquam magni sit ratione fugit repellat architecto quos totam natus nulla vero vel!
+      </h1>
     </div>
   );
 }
